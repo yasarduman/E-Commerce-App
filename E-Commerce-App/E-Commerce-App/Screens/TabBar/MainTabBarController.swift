75af13614ae97c13222e8ab0eaef4a1d0b9321bf
Yaşar DUMAN <01.yasarduman@gmail.com>
2023-11-17 15:34:55 +0300

    ..

---
+++ E-Commerce-App/E-Commerce-App/Screens/TabBar/MainTabBarController.swift
@@ -0,0 +1,82 @@
+//
+//  MainTabBarController.swift
+//  E-Commerce-App
+//
+//  Created by Yaşar Duman on 17.11.2023.
+//
+
+import UIKit
+
+final class MainTabBarController : UITabBarController {
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Tab bar arka plan rengi
+        tabBar.backgroundColor = .tertiarySystemBackground
+        
+        // Seçili olan öğelerin rengi
+        tabBar.tintColor = .label
+
+        // Seçilmeyen öğelerin rengi
+        tabBar.unselectedItemTintColor = .systemGray
+        
+        viewControllers = [
+            createHomeNC(),
+            createSearchNC(),
+            createCartNC(),
+            createProfileNC()
+        ]
+    }
+    
+    // MARK: - Home Navigation Controller 🏠
+    private func createHomeNC() -> UINavigationController {
+        let homeVC = HomeVC()
+        
+        homeVC.tabBarItem = UITabBarItem(title: "Home",
+                                         image: UIImage(systemName: "house"),
+                                         tag: 0)
+        
+        homeVC.tabBarItem.selectedImage = UIImage(systemName: "house.fill")
+        
+        return UINavigationController(rootViewController: homeVC)
+    }
+    
+    // MARK: - Search Navigation Controller 🔍
+    private func createSearchNC() -> UINavigationController {
+        let searcVC = SearchVC()
+        
+        searcVC.tabBarItem = UITabBarItem(title: "Search",
+                                          image: UIImage(systemName: "magnifyingglass"),
+                                          tag: 1)
+        
+        return UINavigationController(rootViewController: searcVC)
+    }
+    
+    // MARK: - Shopping Cart Navigation Controller 🛒
+    private func createCartNC() -> UINavigationController {
+        let cartVC = CartVC()
+        
+        cartVC.tabBarItem = UITabBarItem(title: "Cart",
+                                         image: UIImage(systemName: "cart"),
+                                         tag: 2)
+        
+        cartVC.tabBarItem.selectedImage = UIImage(systemName: "cart.fill")
+        
+        return UINavigationController(rootViewController: cartVC)
+    }
+    
+    // MARK: - Profile Navigation Controller ⚙️
+    private func createProfileNC() -> UINavigationController {
+        let profileVC = ProfileVC()
+        
+        profileVC.tabBarItem = UITabBarItem(title: "Profile",
+                                            image: UIImage(systemName: "person"),
+                                            tag: 3)
+        
+        profileVC.tabBarItem.selectedImage = UIImage(systemName: "person.fill")
+        
+        return UINavigationController(rootViewController: profileVC)
+    }
+}
